8b25ad0312790bbfa04150f7e48767244257ed54
Filip Glavota <filipglavota@Filips-MacBook-Pro.local>
2021-11-08 12:25:33 +0100

    fixed router, get/post works

--- src/post/post.router.js
+++ src/post/post.router.js
@@ -11,7 +11,7 @@ connection = typeorm.createConnection({
    "port": 54320, 
    "username": "postgres", 
    "password": "cibona2!", 
-   "database": "typeorm_charity_db",
+   "database": "typeorm_post_db",
    "synchronize": true, 
    "logging": false, 
    entities: [ new EntitySchema(require("../entity/post.json")) 
@@ -22,12 +22,12 @@ const postController = PostController();
 
 const router = Router();
 
-router.post('/charities', postController.createCharity);
-router.get('/charities/:id', postController.getCharityById);
-router.get('/charities/funds/:id', postController.getCharityFundsById);
-router.get('/charities', postController.getAllCharities);
-router.put('/charities/:id', postController.updateCharity);
-router.put('/charities/funds/:id', postController.updateCharityFunds);
-router.delete('/charities/:id', postController.deleteCharity);
+router.post('/posts', postController.createPost);
+//router.get('/posts/:id', postController.getPostById);
+//router.get('/posts/funds/:id', postController.getPostFundsById);
+router.get('/posts', postController.getAllPosts);
+router.put('/posts/:id', postController.updatePost);
+router.put('/posts/funds/:id', postController.updatePostFunds);
+router.delete('/posts/:id', postController.deletePost);
 
 module.exports = router;
