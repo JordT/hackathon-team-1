970aebbbe7e451bfccffc4f18ef34079ba916375
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-11-29 14:04:01 +0000

    added media

---
+++ sharecare-media/src/index.js
@@ -0,0 +1,16 @@
+// REMOVE THIS COMMENT
+
+const express = require("express");
+const MediaRouter = require('./media/media.router');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/', MediaRouter)
+
+const port = process.env.PORT || "3000";
+
+app.listen(port, () => {    
+    console.log(`Server Running at ${port} 🚀`);
+});
