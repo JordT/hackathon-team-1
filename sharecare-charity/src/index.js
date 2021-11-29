9296df3edb6235dfc5b7ec0cb08f820b1e5c2297
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-11-29 14:02:39 +0000

    added charity

---
+++ sharecare-charity/src/index.js
@@ -0,0 +1,16 @@
+// REMOVE THIS COMMENT
+
+const express = require("express");
+const CharitiesRouter = require('./charities/charity.router');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/', CharitiesRouter)
+
+const port = process.env.PORT || "3000";
+
+app.listen(port, () => {    
+    console.log(`Server Running at ${port} 🚀`);
+});
